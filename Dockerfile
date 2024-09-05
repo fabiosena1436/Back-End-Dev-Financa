01e88d08f3c0186acd28d5f9bad3e48f5c2ec3ec
fabiosena1436 <128174066+fabiosena1436@users.noreply.github.com>
2024-09-05 16:33:24 -0300

    continuando configuraçao do mongo

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM node:20-alpine
+FROM node:18-alpine
 
 WORKDIR /home/app
 
-COPY . . /
+COPY . ./
 
 RUN npm i
 
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,32 +1,16 @@
-// import mongoose from 'mongoose';
-
-// export async function setupMongo(): Promise<void> {
-//   try {
-//     if (mongoose.connection.readyState === 1) {
-//       return;
-//     }
-//     console.log('Connecting to DB...');
-//     await mongoose.connect(process.env.MONGO_URL as string);
-//     console.log('DB Connected!');
-//   } catch {
-//     throw new Error('DB not connected');
-//   }
-// }
-
 import mongoose from 'mongoose';
 
 export async function setupMongo(): Promise<void> {
   try {
     if (mongoose.connection.readyState === 1) {
-      console.log('Database already connected')
       return;
     }
     console.log('Connecting to DB...');
-    console.log('MONGO_URL:', process.env.MONGO_URL);
     await mongoose.connect(process.env.MONGO_URL as string);
     console.log('DB Connected!');
-  } catch (error) {
-    console.error('Error connecting to DB:', error);
+  } catch {
     throw new Error('DB not connected');
   }
 }
+
+
